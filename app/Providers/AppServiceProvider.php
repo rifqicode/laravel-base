3f44f604be360f5a89a712919075eb7e4d3eeb10
rifqicode <muhammadrifqi.tb@gmail.com>
2019-01-09 17:56:47 +0700

    add laratrust

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Events\Dispatcher;
+use JeroenNoten\LaravelAdminLte\Events\BuildingMenu;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,9 +13,18 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(Dispatcher $events)
     {
-        //
+      $events->listen(BuildingMenu::class, function (BuildingMenu $event) {
+          $event->menu->add('MAIN NAVIGATION');
+          $event->menu->add([
+              'text'        => 'Users',
+              'url'         => 'admin/users',
+              'icon'        => 'users',
+              'label'       => '',
+              'label_color' => 'success',
+          ]);
+      });
     }
 
     /**
--- config/adminlte.php
+++ config/adminlte.php
@@ -107,32 +107,7 @@ return [
     |
     */
 
-    'menu' => [
-        'MAIN NAVIGATION',
-        [
-            'text' => 'Blog',
-            'url'  => 'admin/blog',
-            'can'  => 'manage-blog',
-        ],
-        [
-            'text'        => 'Pages',
-            'url'         => 'admin/pages',
-            'icon'        => 'file',
-            'label'       => 4,
-            'label_color' => 'success',
-        ],
-        'ACCOUNT SETTINGS',
-        [
-            'text' => 'Profile',
-            'url'  => 'admin/settings',
-            'icon' => 'user',
-        ],
-        [
-            'text' => 'Change Password',
-            'url'  => 'admin/settings',
-            'icon' => 'lock',
-        ]
-    ],
+    'menu' => [],
 
     /*
     |--------------------------------------------------------------------------
